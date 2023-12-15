56b898c965bbb959e301c3472c4137f655e77c41
Thony3ds <hiphop.antho@gmail.com>
2023-12-15 20:41:50 +0100

    update

--- main.py
+++ main.py
@@ -1,52 +1,110 @@
 import pygame
-from assets.libs import button
+from pygame.locals import *
 
 pygame.init()
-pygame.display.set_caption("Geometry_de")
-screen = pygame.display.set_mode((1080, 720))
+
+screen_width = 600
+screen_height = 600
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Button Demo')
+
+font = pygame.font.SysFont('Constantia', 30)
+
 bg = pygame.image.load("assets/images/background/background.png")
 
-clock = pygame.time.Clock()
-running = True
+# define colours
+bg_color = (204, 102, 0)
+red = (255, 0, 0)
+black = (0, 0, 0)
+white = (255, 255, 255)
 
-#define font
-font = pygame.font.SysFont("arialblack", 40)
+# define global variable
+clicked = False
+counter = 0
 
-#define colors
-text_col = (255, 255, 255)
 
-#load button images
-resume_img = pygame.image.load("assets/images/logo/settings.png")
+class button():
+    # colours for button and text
+    button_col = (255, 0, 0)
+    hover_col = (75, 225, 255)
+    click_col = (50, 150, 255)
+    text_col = black
+    width = 180
+    height = 70
 
-#create button instance
-resume_button = button.Button(304, 125, resume_img, 1)
+    def __init__(self, x, y, text):
+        self.x = x
+        self.y = y
+        self.text = text
 
-def draw_text(text, font, text_col, x, y):
-    img = font.render(text, True, text_col)
-    screen.blit(img, (x, y))
+    def draw_button(self):
 
-while running:
+        global clicked
+        action = False
 
-    #couleur de l'arrière plan mettre la couleur avant car sinon le bg sera derière la couleur
-    screen.fill((255, 0, 0))
+        # get mouse position
+        pos = pygame.mouse.get_pos()
 
-    resume_button.draw(screen)
-    
-    #ajout de l'arrière plan dans l'app
+        # create pygame Rect object for the button
+        button_rect = Rect(self.x, self.y, self.width, self.height)
+
+        # check mouseover and clicked conditions
+        if button_rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1:
+                clicked = True
+                pygame.draw.rect(screen, self.click_col, button_rect)
+            elif pygame.mouse.get_pressed()[0] == 0 and clicked == True:
+                clicked = False
+                action = True
+            else:
+                pygame.draw.rect(screen, self.hover_col, button_rect)
+        else:
+            pygame.draw.rect(screen, self.button_col, button_rect)
+
+        # add shading to button
+        pygame.draw.line(screen, white, (self.x, self.y), (self.x + self.width, self.y), 2)
+        pygame.draw.line(screen, white, (self.x, self.y), (self.x, self.y + self.height), 2)
+        pygame.draw.line(screen, black, (self.x, self.y + self.height), (self.x + self.width, self.y + self.height), 2)
+        pygame.draw.line(screen, black, (self.x + self.width, self.y), (self.x + self.width, self.y + self.height), 2)
+
+        # add text to button
+        text_img = font.render(self.text, True, self.text_col)
+        text_len = text_img.get_width()
+        screen.blit(text_img, (self.x + int(self.width / 2) - int(text_len / 2), self.y + 25))
+        return action
+
+
+again = button(75, 200, 'Play Again?')
+quit = button(325, 200, 'Quit?')
+down = button(75, 350, 'Down')
+up = button(325, 350, 'Up')
+
+run = True
+while run:
+
+    screen.fill(bg_color)
     screen.blit(bg, (0, 0))
 
-    draw_text("Welcome", font, text_col, 540, 460)
+    if again.draw_button():
+        print('Again')
+        counter = 0
+    if quit.draw_button():
+        print('Quit')
+    if up.draw_button():
+        print('Up')
+        counter += 1
+    if down.draw_button():
+        print('Down')
+        counter -= 1
 
-    # pygame.QUIT event means the user clicked X to close your window
+    counter_img = font.render(str(counter), True, red)
+    screen.blit(counter_img, (280, 450))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            run = False
 
-    # RENDER YOUR GAME HERE
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    clock.tick(90)  # limits FPS to 90
+    pygame.display.update()
 
 pygame.quit()
