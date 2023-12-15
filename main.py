e1bf95dd4d60409c20eb5a12c378487f3948f90a
Thony3ds <hiphop.antho@gmail.com>
2023-12-15 20:50:39 +0100

    update

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ screen_width = 600
 screen_height = 600
 
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Button Demo')
+pygame.display.set_caption('Geometry of the cube')
 
-font = pygame.font.SysFont('Constantia', 30)
+font = pygame.font.SysFont('arialblack', 30)
 
 bg = pygame.image.load("assets/images/background/background.png")
 
@@ -21,8 +21,6 @@ white = (255, 255, 255)
 
 # define global variable
 clicked = False
-counter = 0
-
 
 class button():
     # colours for button and text
@@ -77,8 +75,6 @@ class button():
 
 again = button(75, 200, 'Play Again?')
 quit = button(325, 200, 'Quit?')
-down = button(75, 350, 'Down')
-up = button(325, 350, 'Up')
 
 run = True
 while run:
@@ -91,15 +87,6 @@ while run:
         counter = 0
     if quit.draw_button():
         print('Quit')
-    if up.draw_button():
-        print('Up')
-        counter += 1
-    if down.draw_button():
-        print('Down')
-        counter -= 1
-
-    counter_img = font.render(str(counter), True, red)
-    screen.blit(counter_img, (280, 450))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
